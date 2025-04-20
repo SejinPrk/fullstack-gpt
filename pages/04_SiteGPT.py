5e49f8b9b9241b740b6ea066dc87288151469333
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2025-04-20 17:36:17 +0900

    feat: SitemapLoader

--- pages/04_SiteGPT.py
+++ pages/04_SiteGPT.py
@@ -1,13 +1,20 @@
-from langchain.document_loaders import AsyncChromiumLoader
+from langchain.document_loaders import SitemapLoader
 from langchain.document_transformers import Html2TextTransformer
 import streamlit as st
+
+@st.cache_data(show_spinner="Loading website...")
+def load_website(url):
+    loader = SitemapLoader(url)
+    loader.requests_per_second = 5
+    docs = loader.load() 
+    return docs
 
 st.set_page_config(
   page_title="SiteGPT",
   page_icon="🌐",
 )
 
-html2text_transformer = Html2TextTransformer()
+# html2text_transformer = Html2TextTransformer()
 
 st.markdown(
     """
@@ -27,7 +34,15 @@ with st.sidebar:
 
 if url:
   # async chromium loader
-  loader = AsyncChromiumLoader([url])
-  docs = loader.load()
-  transformed = html2text_transformer.transform_documents(docs)
-  st.write(docs)
+  # loader = AsyncChromiumLoader([url])
+  # docs = loader.load()
+  # transformed = html2text_transformer.transform_documents(docs)
+  # st.write(docs)
+
+  # sync sitemap loader
+  if ".xml" not in url:
+    with st.sidebar:
+      st.error("Please write down a Sitemap URL")
+  else:
+    docs = load_website(url)
+    st.write(docs)
