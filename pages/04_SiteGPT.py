0149aa0fd3b7b8ff564dcc679d0fa5aee566cbf6
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2025-04-20 21:47:59 +0900

    feat: Map ReRank Chain

--- pages/04_SiteGPT.py
+++ pages/04_SiteGPT.py
@@ -1,7 +1,99 @@
 from langchain.document_loaders import SitemapLoader
+from langchain.schema.runnable import RunnableLambda
 from langchain.document_transformers import Html2TextTransformer
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.prompts import ChatPromptTemplate
 import streamlit as st
+
+llm = ChatOpenAI(
+  temperature=0.1,
+) 
+
+answers_prompt = ChatPromptTemplate.from_template("""
+Using ONLY the following context answer the user's question. If you can't just say you don't know, don't make anything up.
+                                                  
+    Then, give a score to the answer between 0 and 5.
+
+    If the answer answers the user question the score should be high, else it should be low.
+
+    Make sure to always include the answer's score even if it's 0.
+
+    Context: {context}
+                                                  
+    Examples:
+                                                  
+    Question: How far away is the moon?
+    Answer: The moon is 384,400 km away.
+    Score: 5
+                                                  
+    Question: How far away is the sun?
+    Answer: I don't know
+    Score: 0
+                                                  
+    Your turn!
+
+    Question: {question}
+  """
+)
+
+def get_answers(inputs):
+  docs = inputs["docs"]
+  question = inputs["question"]
+  answers_chain = answers_prompt | llm
+  # answers = []
+  # for doc in docs:
+  #   result = answers_chain.invoke({
+  #     "context": doc.page_content,
+  #     "question": question
+  #   })
+  #   answers.append(result.content)
+  return {
+    "question": question,
+    "answers":[
+    {
+      "answer": answers_chain.invoke(
+        { "question": question, "context": doc.page_content }
+      ).content,
+      "source": doc.metadata["source"],
+      "date": doc.metadata["lastmod"],
+    } 
+    for doc in docs
+    ]
+  }
+
+choose_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            Use ONLY the following pre-existing answers to answer the user's question.
+
+            Use the answers that have the highest score (more helpful) and favor the most recent ones.
+
+            Cite sources and return the sources of the answers as they are, do not change them.
+
+            Answers: {answers}
+            """,
+        ),
+        ("human", "{question}"),
+    ]
+)
+
+def choose_answer(inputs):
+  answers = inputs["answers"]
+  question = inputs["question"]
+  choose_chain = choose_prompt | llm
+  condensed = "\n\n".join(f"Answer: {answer['answer']}\nSource: {answer['source']}\nDate: {answer['date']}\n\n" for answer in answers)
+  for answer in answers:
+    condensed += f"Answer: {answer['answer']}\nSource: {answer['source']}\nDate: {answer['date']}\n\n"
+  st.write(condensed)
+  choose_chain.invoke({
+    "question": question,
+    "answer": condensed
+  })
 
 def parse_page(soup):
   header = soup.find("header")
@@ -17,7 +109,7 @@ def parse_page(soup):
     .replace("CloseSearch Submit Blog", " ")
   )
 
-@st.cache_data(show_spinner="Loading website...")
+@st.cache_resource(show_spinner="Loading website...")
 def load_website(url):
     splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
       chunk_size=1000,
@@ -25,14 +117,12 @@ def load_website(url):
     )
     loader = SitemapLoader(
       url,
-      filter_urls=[
-          r"^(.*\/blog\/).*",
-        ],
-        parsing_function = parse_page,
+      parsing_function = parse_page,
       )
     loader.requests_per_second = 5
     docs = loader.load_and_split(text_splitter=splitter) 
-    return docs
+    vector_store = FAISS.from_documents(docs, OpenAIEmbeddings())
+    return vector_store.as_retriever()
 
 st.set_page_config(
   page_title="SiteGPT",
@@ -69,5 +159,14 @@ if url:
     with st.sidebar:
       st.error("Please write down a Sitemap URL")
   else:
-    docs = load_website(url)
-    st.write(docs)
+    retriever = load_website(url)
+    query = st.text_input("Ask a question to the website")
+    if query:
+      chain = {
+        "docs": retriever,
+        "question": RunnableLambda(lambda x: x),
+      }| RunnableLambda(get_answers) | RunnableLambda(choose_answer)
+
+    result = chain.invoke(query)
+    st.write(result.content)
+    
